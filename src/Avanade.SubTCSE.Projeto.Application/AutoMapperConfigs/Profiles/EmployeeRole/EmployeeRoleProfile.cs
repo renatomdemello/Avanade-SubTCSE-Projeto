228d3c739c40477eff3f671a1a03298cd8644af5
renatomdemello <renatomdemello@gmail.com>
2021-06-21 09:06:03 -0300

    Implementação das interfaces.

---
+++ src/Avanade.SubTCSE.Projeto.Application/AutoMapperConfigs/Profiles/EmployeeRole/EmployeeRoleProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+
+namespace Avanade.SubTCSE.Projeto.Application.AutoMapperConfigs.Profiles.EmployeeRole
+{
+    public class EmployeeRoleProfile : Profile
+    {
+        public EmployeeRoleProfile()
+        {
+            CreateMap<Dtos.EmployeeRole.EmployeeRoleDto, Domain.Aggregates.EmployeeRole.Entities.EmployeeRole>()
+                .ForCtorParam("roleName", opt => opt.MapFrom(src => src.Cargo));
+
+            //CreateMap<Domain.Aggregates.EmployeeRole.Entities.EmployeeRole, Dtos.EmployeeRole.EmployeeRoleDto>()
+                //.ForMember();
+        }
+    }
+}
